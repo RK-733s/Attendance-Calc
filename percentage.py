bee1c9ff76595f94b9aba42243506b2845c0712e
RAJKUMAR <121594446+RK-733s@users.noreply.github.com>
2023-11-25 08:43:01 +0530

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -7,19 +7,20 @@ def main():
     w_days=st.number_input("Enter Total Days :",value=0)
     a_days=st.number_input("Enter Absent Days :",value=0)
    
-    data = {'TotalDays': [w_days], 'AbsentDays': [a_days]}
+    data = {'TotalDays': [w_days],'Present Days':[w_days-a_days], 'AbsentDays': [a_days]}
     df = pd.DataFrame(data)
    
 
     if st.button("Submit"):
         st.write("Total Days = ",w_days)
+        st.write("Present Days = ",w_days-a_days)
         st.write("Absent Days = ",a_days)
         per=(w_days-a_days)/w_days*100
         st.write("Your Total Percentage ",per,"%")
     
         fig, ax = plt.subplots()
         ax.bar(df.columns, df.iloc[0])
-        ax.set_ylabel('Days')
+        ax.set_ylabel('Total Days')
         st.pyplot(fig)
         
 if __name__ == '__main__':
